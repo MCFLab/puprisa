61e157823ce1eb913f9be62b2735a9622a2804cb
Xiaotian Feng <xf58@duke.edu>
2026-08-26 18:30:00 -0400

    add app icon

--- src/puprisa/gui.py
+++ src/puprisa/gui.py
@@ -1,9 +1,12 @@
 import sys
+from pathlib import Path
 from PySide6.QtWidgets import QApplication
+from PySide6.QtGui import QIcon
 from puprisa.app_context import ApplicationContext
 
 def main():
     app = QApplication(sys.argv)
+    app.setWindowIcon(QIcon(str(Path(__file__).parent / "ui" / "resources" / "qt.svg")))
     ctx = ApplicationContext()
     main_win = ctx.create_main_window()
     main_win.show()
